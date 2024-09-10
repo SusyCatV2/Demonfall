e072f3e005b2f92363e156221ade9e548a882995
SusyCatV2 <156109947+SusyCatV2@users.noreply.github.com>
2024-09-10 17:17:21 +0300

    Update Godmode.lua

--- Godmode.lua
+++ Godmode.lua
@@ -1,5 +1,5 @@
 -- Obfuscated
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/SusyCatV2/Demonfall/main/Godmode%20Obfuscated.lua?token=GHSAT0AAAAAACXJRXSBJJZRQRCSSIRHDBHSZXAKLKQ"),true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/SusyCatV2/Demonfall/main/Godmode%20Obfuscated.lua"),true))()
 
 -- Unobfuscated
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/SusyCatV2/Demonfall/main/Godmode%20Unobfuscated.lua"),true))()
