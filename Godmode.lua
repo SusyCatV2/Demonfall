db6b870d693ce991177a5ffade6f8c286e5f17b4
SusyCatV2 <156109947+SusyCatV2@users.noreply.github.com>
2024-09-10 17:09:21 +0300

    Create Godmode.lua

---
+++ Godmode.lua
@@ -0,0 +1,5 @@
+-- Obfuscated
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/SusyCatV2/Demonfall/main/Godmode%20Obfuscated.lua?token=GHSAT0AAAAAACXJRXSBJJZRQRCSSIRHDBHSZXAKLKQ"),true))()
+
+-- Unobfuscated
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/SusyCatV2/Demonfall/main/Godmode%20Unobfuscated.lua?token=GHSAT0AAAAAACXJRXSBTRYYP6ABWYBCLOEAZXAKKAA"),true))()
